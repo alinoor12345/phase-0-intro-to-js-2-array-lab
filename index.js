7f60438e0a1111b46c4d886276680e05235d4e98
Ali Omar <ali.noor.student.moringaschool.com git config --gobal Ali abdullahigit config --global user.email ali.noor.student.moringaschool.com>
2022-12-05 12:35:17 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop("Ralph");
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift("Bob");
+}
+
+function appendCat(name){
+    const cats2 = cats.slice();
+    cats2.push(name)
+    return cats2
+}
+
+function prependCat(name){
+    const cats2 = cats.slice()
+    cats2.unshift(name);
+    return cats2
+}
+
+function removeLastCat(name){
+    const cats2 = cats.slice()
+    cats2.pop();
+    return cats2;
+}
+
+function removeFirstCat(name){
+    const cats2 = cats.slice()
+    cats2.shift()
+    return cats2
+}
+Footer
